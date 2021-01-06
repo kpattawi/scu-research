cb28c4dca4eaf5471eba6d1fa67c6795d8c1ca12
Kaleb Pattawi <72418535+kpattawi@users.noreply.github.com>
2021-01-05 22:16:12 -0800

    Added runperiod to energyPlus_config

--- energyPlus_config/Config.py
+++ energyPlus_config/Config.py
@@ -1,15 +1,22 @@
 import sys
 
-def Config(idf,searchExp,replaceExp):
+def Config(idf,searchExp,replaceExp,numLines):
     new_idf=""
+    counter = 0
     with open(idf) as openfile:
         for line in openfile:
+            if counter>0 and counter<numLines:
+                line = ""
+                counter += 1
             if searchExp in line:
                 print (line)
-                check = input("Replace this line to [" + replaceExp + "]? (Enter/No)")
-                if check == "":
-                    print ("Replaced")
-                    line=replaceExp + "\n"
+                # check = input("Replace this line to [" + replaceExp + "]? (Enter/No)")
+                # if check == "":
+                #     print ("Replaced")
+                #     line=replaceExp + "\n"
+                line = replaceExp + "\n"
+                counter += 1
+
             new_idf += line
 
     idf2 = open(idf,"w")
@@ -18,11 +25,26 @@ def Config(idf,searchExp,replaceExp):
 
 #==============================================================================
 #============================= Input Everything here ==========================
-idfname = "1.idf"
-Keyword = "Timestep,"
-NewParameter = "Timestep,6;"
+folderPath = "C:/Users\kpattawi\Desktop\Buildings\TestConfig"   # CANT HAVE \U for some reason so change \Users.. to /Users..
+idfname = "SF_California_San.Francisco.Intl.AP.724940_gasfurnace_crawlspace_IECC_2012.idf"
+newTimestep = 12
+newStartMonth = 1
+newStartDay = 1
+newEndMonth =  12
+newEndDay = 31
+numberOfIDFs = 3
 #===============================================================================
 #===============================================================================
 
-for i in range(1,4):
-    Config("./"+ str(i) + "/" +idfname,Keyword,NewParameter) # Change parameter here! 
+Keyword1 = "Timestep,"
+NewParameter1 = "Timestep,\n"+str(newTimestep)+";"
+numLines1 = 2   # this is the number of lines that are in the "Timestep" class in EnergyPlus
+
+Keyword2 = "RunPeriod,"
+NewParameter2 = "RunPeriod,\nAnnual,\n"+str(newStartMonth)+",\n"+str(newStartDay)+",\n,\n"+str(newEndMonth)+",\n"+str(newEndDay)+",\n,\nUseWeatherFile,\nYes,\nYes,\nNo,\nYes,\nYes;"
+numLines2 = 14   # this is number of lines that are in the "RunPeriod" class in EnergyPlus
+
+
+for i in range(1,numberOfIDFs+1):
+    Config(folderPath+ "/" +str(i) + "/" +idfname,Keyword1,NewParameter1,numLines1) # Change parameter here!
+    Config(folderPath+ "/" +str(i) + "/" +idfname,Keyword2,NewParameter2,numLines2) # Change parameter here!
